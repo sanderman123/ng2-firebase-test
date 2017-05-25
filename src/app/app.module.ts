a09e0c74239d4e7e6fcfff32902a9071dc42a756
Sander <sander@ubuntu-desktop>
2017-05-25 21:01:48 +0200

    adds firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCoRfidXM4HFAV3OKUTzJP2ZDVzgTEOouQ',
+  authDomain: 'ngfbauth-628cf.firebaseapp.com',
+  databaseURL: 'https://ngfbauth-628cf.firebaseio.com',
+  projectId: 'ngfbauth-628cf',
+  storageBucket: 'ngfbauth-628cf.appspot.com',
+  messagingSenderId: '874018556575'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
