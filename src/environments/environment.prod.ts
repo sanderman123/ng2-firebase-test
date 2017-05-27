990049a48b5fad7f086e7de9398996b6da9e6693
Sander <sander@ubuntu-desktop>
2017-05-27 22:31:28 +0200

    travis build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCoRfidXM4HFAV3OKUTzJP2ZDVzgTEOouQ',
+    authDomain: 'ngfbauth-628cf.firebaseapp.com',
+    databaseURL: 'https://ngfbauth-628cf.firebaseio.com',
+    projectId: 'ngfbauth-628cf',
+    storageBucket: 'ngfbauth-628cf.appspot.com',
+    messagingSenderId: '874018556575'
+  }
 };
