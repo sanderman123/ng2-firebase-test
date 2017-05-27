c713b7e8eb1b50c8b1c1a7d6e2eb3b7bb2763f1d
Sander <sander@ubuntu-desktop>
2017-05-27 18:04:04 +0200

    mutate and query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,29 @@ export class AppComponent {
   item: FirebaseObjectObservable<any>;
 
   constructor(db: AngularFireDatabase) {
-    this.items = db.list('/items');
+    this.items = db.list('/items', {
+      query: {
+        orderByValue: true
+      }
+    });
     this.item = db.object('/item');
+  }
+
+  remove(item: FirebaseObjectObservable<any>) {
+    console.log('remove:', item);
+    item.remove()
+      .then(_ => console.log('success'))
+      .catch(err => console.log('Error', err));
+
+    // item.set({'name': 'new name'})
+    //   .then(_ => console.log('settings success'))
+    //   .catch(err => console.log('error', err));
+  }
+
+  setName(item: FirebaseObjectObservable<any>, newName: string) {
+    console.log('set:', item, 'newname', newName);
+    item.set({name: newName})
+      .then(_ => console.log('success'))
+      .catch(err => console.log('Error', err));
   }
 }
